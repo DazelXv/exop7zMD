68d301f75bca5aac41c3a8fd9c2d852055b6f9a0
DazélXv <dazelgepete@gmail.com>
2026-03-14 18:17:38 +0700

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,9 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['6283866051638']
+global.namaowner = 'name¹⁴'
+
+//----ALL PAYMENT----//
+global.dana = ""
+global.gopay = ""
+global.ovo = false
+global.qris = false
